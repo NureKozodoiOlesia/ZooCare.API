3c0ab970fc13d9489de80092e7da522eb3d02633
Alesia <alesiakozodoi@gmail.com>
2025-12-27 05:33:09 +0200

    deploy

--- ZooCare.API/Program.cs
+++ ZooCare.API/Program.cs
@@ -100,11 +100,8 @@ catch (Exception ex)
 }
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 // CORS має бути на початку pipeline, перед UseHttpsRedirection
 app.UseCors("AllowAll");
